e369d35fb9bfd13fc9e35bc9eb66cb82187c7175
daffapriyantana <141655845+daffapriyantana@users.noreply.github.com>
2025-03-12 20:47:03 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,42 +1,44 @@
-//class hewan
-class Hewan {
+import java.util.Scanner;
+import  java.time.LocalDate;
 
-    //atribut
-    String Nama;
-    String Jenis;
-    String Suara;
-    String Kulit;
-
-    //method menampilkan informasi hewan
-    void tampilkanInfo() {
-        System.out.println("Nama: " + Nama);
-        System.out.println("Jenis: " + Jenis);
-        System.out.println("Suara: " + Suara);
-        System.out.println("Kulit: " + Kulit);
-        System.out.println(); // baris kosong untuk pemisah
-    }
-}
-
-//kelas main
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        // Membuat Scanner untuk membaca input
+        Scanner scanner = new Scanner(System.in);
 
-        //membuat objek hewan1
-        Hewan hewan1 = new Hewan();
-        hewan1.Nama = "Harimau";
-        hewan1.Jenis = "Mamalia";
-        hewan1.Suara = "RAWRRRR";
-        hewan1.Kulit = "loreng hitam oren";
+        // Meminta pengguna memasukkan data
+        System.out.print("Masukkan Nama : ");
+        String nama = scanner.nextLine();
 
-        //membuat objek hewan2
-        Hewan hewan2 = new Hewan();
-        hewan2.Nama = "Sapi";
-        hewan2.Jenis = "Mamalia";
-        hewan2.Suara = "MOOOOOOO";
-        hewan2.Kulit = "hitam putih";
+        System.out.print("Masukkan jenis kelamin (P/L) : ");
+        char jenisKelaminInput = scanner.next().charAt(0);
 
-        //Menampilkan informasi hewan 1 dan hewan 2
-        hewan1.tampilkanInfo();
-        hewan2.tampilkanInfo();
+        System.out.print(" Masukkan tahun lahir : ");
+        int tahunLahir = scanner.nextInt();
+
+        // Menutup scanner
+        scanner.close();
+
+        // Menyesuaikan dengan tahun sekarang
+        int tahunSekarang = LocalDate.now().getYear();
+
+        // Menghitung umur
+        int umur = tahunSekarang - tahunLahir;
+
+        // Menentukan jenis kelamin
+        String jenisKelamin;
+        if (jenisKelaminInput == 'L' || jenisKelaminInput == 'l') {
+            jenisKelamin = "Laki-laki";
+        } else if (jenisKelaminInput == 'p' || jenisKelaminInput == 'p') {
+            jenisKelamin = "Perempuan";
+        } else {
+            jenisKelamin = "Tidak valid ";
+        }
+
+        // Menampilkan output
+        System.out.println("\nData Diri :");
+        System.out.println("Nama : " + nama);
+        System.out.println("Jenis kelamin : " + jenisKelamin);
+        System.out.println("Umur : " + umur + "tahun");
     }
 }
